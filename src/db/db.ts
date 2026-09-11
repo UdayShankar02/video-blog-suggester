ae775e10bff08f786b7afb3c9fca137a14d5ce36
Shankar C <uday.shankar.c@accenture.com>
2026-09-11 15:55:22 +0530

    Setup Drizzle with Neon db

---
+++ src/db/db.ts
@@ -0,0 +1,6 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { serverEnv } from '../../data/serverEnv';
+import {relations} from './relations';
+
+export const db = drizzle(serverEnv.DATABASE_URL, {relations});
+
