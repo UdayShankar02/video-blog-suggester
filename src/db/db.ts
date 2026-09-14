0189e70047357d8da795f44d0252fe854ffa35c5
Uday Shankar C <udaychandrulc@gmail.com>
2026-09-14 19:50:44 +0530

    Minor changes to db and npm installs

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,5 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { serverEnv } from '../../data/serverEnv';
-import {relations} from './relations';
+import { drizzle } from "drizzle-orm/neon-http"
+import { serverEnv } from "../data/serverEnv"
+import { relations } from "./relations"
 
-export const db = drizzle(serverEnv.DATABASE_URL, {relations});
-
+export const db = drizzle(serverEnv.DATABASE_URL, { relations })
